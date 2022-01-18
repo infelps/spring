e4a2505e2cc8e220f5a0dbee516015aaa28bc440
infelps <11seongmin@naver.com>
2022-01-18 15:41:35 +0900

    add helloDto가_리턴된다

--- src/main/java/com/infelps/book/springBoot/web/HelloController.java
+++ src/main/java/com/infelps/book/springBoot/web/HelloController.java
@@ -1,5 +1,7 @@
 package com.infelps.book.springBoot.web;
+import com.infelps.book.springBoot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,5 +9,10 @@ public class HelloController{
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,@RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
