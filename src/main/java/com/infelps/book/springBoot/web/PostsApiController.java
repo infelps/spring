e849b80446694c5630406b92debc09bb1401caa1
infelps <11seongmin@naver.com>
2022-01-24 16:38:54 +0900

    게시글 등록

---
+++ src/main/java/com/infelps/book/springBoot/web/PostsApiController.java
@@ -0,0 +1,29 @@
+package com.infelps.book.springBoot.web;
+import com.infelps.book.springBoot.service.posts.PostsService;
+import com.infelps.book.springBoot.web.dto.PostsResponseDto;
+import com.infelps.book.springBoot.web.dto.PostsSaveRequestDto;
+import com.infelps.book.springBoot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
---
+++ src/test/java/com/infelps/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.infelps.book.springboot.domain.posts;
+import com.infelps.book.springBoot.domain.posts.Posts;
+import com.infelps.book.springBoot.domain.posts.PostsRepository;
+import javafx.geometry.Pos;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.constraints.PositiveOrZero;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepsitory;
+
+    @After
+    public void cleanup(){
+        postsRepsitory.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepsitory.save(Posts.builder().title(title).content(content).author("infelps@gmail.com").build());
+
+        List<Posts> postsList = postsRepsitory.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        LocalDateTime now=LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepsitory.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList=postsRepsitory.findAll();
+
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>>createDate="+posts.getCreatedDate()+",modifiedDate="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+    }
+}
