f35f6309092de837ad8fa145356ea73f597a636f
infelps <11seongmin@naver.com>
2022-01-28 17:20:25 +0900

    add naver oauth login

---
+++ src/main/java/com/infelps/book/springBoot/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.infelps.book.springBoot.config;
+
+import com.infelps.book.springBoot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
--- src/main/java/com/infelps/book/springBoot/web/IndexController.java
+++ src/main/java/com/infelps/book/springBoot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.infelps.book.springBoot.web;
 
+import com.infelps.book.springBoot.config.auth.LoginUser;
 import com.infelps.book.springBoot.config.auth.dto.SessionUser;
 import com.infelps.book.springBoot.service.posts.PostsService;
 import com.infelps.book.springBoot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user=(SessionUser) httpSession.getAttribute("user");
         if(user!=null){
             model.addAttribute("userName", user.getName());
         }
