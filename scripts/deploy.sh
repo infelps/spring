dcbc0cbc702a7b577b8aee321ca4f891711a211d
infelps <11seongmin@naver.com>
2022-02-22 01:43:38 +0900

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,4 +31,7 @@ chmod +x $JAR_NAME
 
 echo "execute $JAR_NAME"
 
-nohup java jar -Dspring.config.location=classpath:/applcation.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
